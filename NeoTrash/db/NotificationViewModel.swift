af57d9b6abe73e02a3ef9a452345cf05f4ede075
rizki-ramadhanw <rizki.rwsaputra@gmail.com>
2025-12-08 21:09:39 +0700

    commit selesai

---
+++ NeoTrash/db/NotificationViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationViewModel.swift
+//  NeoTrash
+//
+//  Created by Rizki Ramadhan Wira Saputra on 08/12/25.
+//
+
+import SwiftUI
+
+struct NotificationViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationViewModel()
+}
